53378384f167b1f805bf4674f859e94905fd48f7
jrdarwin <junior.ruiz.perez00@gmail.com>
2025-02-17 21:16:29 +0100

    feat: module caching

--- module-scope/main.js
+++ module-scope/main.js
@@ -1,2 +1,23 @@
+console.clear();
+
 require("./superman");
 require("./batman");
+
+// const superHero = require("./super-hero");
+// console.log(superHero.getName());
+
+// superHero.setName("Superman");
+// console.log(superHero.getName());
+
+// const newSuperHero = require("./super-hero");
+// console.log(newSuperHero.getName());
+
+const SuperHero = require("./super-hero");
+
+const batman = new SuperHero("Batman");
+console.log(batman.getName());
+batman.setName("Bruce Wayne");
+console.log(batman.getName());
+
+const superman = new SuperHero("Superman");
+console.log(superman.getName());
