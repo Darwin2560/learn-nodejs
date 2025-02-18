a990d6ac7ca350b9fff72b7dea2fce99895da8d9
jrdarwin <junior.ruiz.perez00@gmail.com>
2025-02-18 17:50:47 +0100

    feat: ES modules

---
+++ es-modules/main.mjs
@@ -0,0 +1,9 @@
+console.clear();
+
+import { add, subtract } from './math-esm.mjs';
+// import * as math from './math-esm.mjs';
+
+// const { add, subtract } = math;
+
+console.log(add(2, 3)); // Output: 5
+console.log(subtract(2, 3)); // Output: -1
